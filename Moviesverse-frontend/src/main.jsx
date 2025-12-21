2c7fb58189108aa98d9259774e194df858c22c11
Balram Dhakad <bdhakad886@gmail.com>
2025-12-21 18:57:24 +0530

    BMC Added 2

--- Moviesverse-frontend/src/main.jsx
+++ Moviesverse-frontend/src/main.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 import "./index.scss";
-
 import { store } from "./store/store";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <Provider store={store}>
-        <App />
-        <Analytics />
-    </Provider>
+  <Provider store={store}>
+    <App />
+    <Analytics />
+  </Provider>
 );
